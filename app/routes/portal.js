cf5cfee5aef9cc1e5b3c02fcbd73d675622cec95
Brian Miller <mills_77_77@hotmail.com>
2018-05-21 13:03:03 -0500

    query for advertiser on portal route

--- app/routes/portal.js
+++ app/routes/portal.js
@@ -1,5 +1,13 @@
 import Route from '@ember/routing/route';
+import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Route.extend(AuthenticatedRouteMixin, {
+import query from 'fortnight/gql/queries/advertiser';
+
+export default Route.extend(AuthenticatedRouteMixin, RouteQueryManager, {
+  model({ id }) {
+    console.log('model', id);
+    const variables = { input: { id } };
+    return this.get('apollo').watchQuery({ query, variables, fetchPolicy: 'network-only' }, 'advertiser');
+  },
 });
