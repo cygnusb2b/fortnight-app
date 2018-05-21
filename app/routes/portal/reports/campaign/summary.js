fd7fd0d48fa1559afdfe84f3f1d7a260fa2eb6bd
Brian Miller <mills_77_77@hotmail.com>
2018-05-21 10:41:30 -0500

    more cleanup

--- app/routes/portal/reports/campaign/summary.js
+++ app/routes/portal/reports/campaign/summary.js
@@ -4,7 +4,6 @@ import RouteQueryManager from 'ember-apollo-client/mixins/route-query-manager';
 import query from 'fortnight/gql/queries/reports/campaign-summary';
 
 export default Route.extend(RouteQueryManager, {
-
   model() {
     const { hash } = this.modelFor('portal.reports.campaign');
     const variables = { input: { hash } };
